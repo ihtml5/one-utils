a0c9ce714fafed840bd4efe8028007ebdc063978
html5@mobile <phpython8@gmail.com>
2018-04-18 07:26:23 +0800

    modify name

--- src/dom/index.js
+++ src/dom/index.js
@@ -1,54 +1,51 @@
-import { getEnv } from '../env';
-import { wx } from '../global';
-import { isFunction } from '../types';
+import { getEnv } from "../env";
+import { wx } from "../global";
+import { isFunction } from "../types";
 
 class WxDomCore {
-    constructor(selector) {
-        this.envName = getEnv();
-        this.selector = selector;
+  constructor(selector) {
+    this.environment = getEnv();
+    this.selector = selector;
+  }
+  querySelector(selector) {
+    this.selector = selector;
+    if (this.environment === "browser") {
+      this.node = document.querySelector(selector);
     }
-    querySelector(selector) {
-        this.selector = selector;
-        if (this.envName === 'browser') {
-            this._selectResult = document.querySelector(selector);
-        }
-        if (this.envName === 'wxapp') {
-            const wxQuery = wx.createSelectorQuery();
-            this._selectResult = wxQuery.select(this.selector);
-        }
-        return this;
+    if (this.environment === "wxapp") {
+      this.node = wx.createSelectorQuery().select(selector);
     }
-    getBoundingClientRect(callback) {
-        let result = null;
-        if (this.envName === 'browser') {
-            if (isFunction(callback)) {
-                callback(this._selectResult.getBoundingClientRect());
-            }
-            return this._selectResult.getBoundingClientRect();
-        }
-        if (this.envName === 'wxapp') {
-            return new Promise((resolve, reject) => {
-                this._selectResult.boundingClientRect().exec(res => {
-                    const { top, left, right, bottom } = res[0];
-                    if (isFunction(callback)) {
-                        callback({
-                            top,
-                            left,
-                            right,
-                            bottom,
-                        });
-                    }
-                    resolve({
-                    {
-                            top,
-                            left,
-                            right,
-                            bottom,
-                        }});
-                });
+    return this;
+  }
+  getBoundingClientRect(callback) {
+    if (this.environment === "browser") {
+      if (isFunction(callback)) {
+        callback(this.node.getBoundingClientRect());
+      }
+      return this.node.getBoundingClientRect();
+    }
+    if (this.environment === "wxapp") {
+      return new Promise((resolve, reject) => {
+        this.node.boundingClientRect().exec(res => {
+          const { top, left, right, bottom } = res[0];
+          if (isFunction(callback)) {
+            callback({
+              top,
+              left,
+              right,
+              bottom
             });
-        }
+          }
+          resolve({
+            top,
+            left,
+            right,
+            bottom
+          });
+        });
+      });
     }
+  }
 }
 
 export default new WxDomCore();
