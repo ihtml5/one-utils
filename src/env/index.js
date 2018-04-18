10f52f5f4501dbe9d20d8b86ed2f98e327ff8c5c
html5@mobile <phpython8@gmail.com>
2018-04-18 14:46:45 +0800

    Update index.js

--- src/env/index.js
+++ src/env/index.js
@@ -1,8 +1,10 @@
+// http://www.harmontan.info/index.php/2015/08/29/detect-global-object-type-and-runtime/
 import { isObject } from '../types';
 export const ENVIRONMENTS = {
     web: '0',
     wxapp: '1',
     node: '2',
+    webworker: 3,
     unknown: 'unknown',
 }
 export const isWxAppEnv = !(!wx || !Page || !App || !Component);
