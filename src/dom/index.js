e8f33ead3f7e38feba6e83f8d39398de9565d8e0
thinkchen <phpython8@gmail.com>
2018-04-18 13:40:01 +0800

    add ENVIRONMENTS

--- src/dom/index.js
+++ src/dom/index.js
@@ -1,4 +1,4 @@
-import { getEnv } from "../env";
+import { getEnv, ENVIRONMENTS } from "../env";
 import { wx } from "../global";
 import { isFunction, isObject } from "../types";
 
@@ -9,32 +9,32 @@ class WxDomCore {
   }
   querySelectorAll(selector) {
     this.selector = selector;
-    if (this.environment === "browser") {
+    if (this.environment === ENVIRONMENTS.web) {
       this.nodeList = document.querySelectorAll(selector);
     }
-    if (this.environment === "wxapp") {
+    if (this.environment === ENVIRONMENTS.wxapp) {
         this.nodeList = wx.createSelectorQuery().selectAll(selector);
     }
     return this;
   }
   querySelector(selector) {
     this.selector = selector;
-    if (this.environment === "browser") {
+    if (this.environment === ENVIRONMENTS.web) {
       this.nodeList = document.querySelector(selector);
     }
-    if (this.environment === "wxapp") {
+    if (this.environment === ENVIRONMENTS.wxapp) {
       this.nodeList = wx.createSelectorQuery().select(selector);
     }
     return this;
   }
   getBoundingClientRect(callback) {
-    if (this.environment === "browser") {
+    if (this.environment === ENVIRONMENTS.web) {
       if (isFunction(callback)) {
         callback(this.nodeList.getBoundingClientRect());
       }
       return this.node.getBoundingClientRect();
     }
-    if (this.environment === "wxapp") {
+    if (this.environment === ENVIRONMENTS.wxapp) {
       return new Promise((resolve, reject) => {
         this.nodeList.boundingClientRect().exec(res => {
           const returnVal = isObject(res[0]) ? res[0] : res;
--- src/env/index.js
+++ src/env/index.js
@@ -1,15 +1,20 @@
 import { isObject } from '../types';
-
+export const ENVIRONMENTS = {
+    web: '0',
+    wxapp: '1',
+    node: '2',
+    unknown: 'unknown',
+}
 export const isWxAppEnv = !(!wx || !Page || !App || !Component);
 export const getEnv = () => {
     if (isWxAppEnv) {
-        return 'wxapp';
+        return ENVIRONMENTS.wxapp;
     }
     if (isObject(window) && window.location) {
-        return 'browser';
+        return  ENVIRONMENTS.web;
     }
     if (isObject(global) && global.setTimeout) {
-        return 'node';
+        return  ENVIRONMENTS.node;
     }
-    return 'unknown';
+    return ENVIRONMENTS.unknown;
 }
